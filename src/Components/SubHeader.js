74d855050b3f1a062825429127accda0417eefe3
Amshitha-Nair <amshithanair0@gmail.com>
2024-02-10 12:21:06 +0530

    Update SubHeader.js

--- src/Components/SubHeader.js
+++ src/Components/SubHeader.js
@@ -7,7 +7,7 @@ const SubHeader = ({barname , openStatus}) => {
   return (
     <Grid container  className='SubHeadCont'>
       <Grid item xs = {0.2}></Grid>
-      <Grid item xs = {10.8}className='SubTxtBox' ><text className='SubHeadtxt'>{barname}</text></Grid>
+      <Grid item xs = {10.8} className='SubTxtBox' ><text className='SubHeadtxt'>{barname}</text></Grid>
       <Grid item xs = {1} >
         {
         openStatus === true ?(<IoTriangleOutline style={{transform:'rotate(0deg)'}}/>):(<IoTriangleOutline style={{transform:'rotate(180deg)'}}/>) 
