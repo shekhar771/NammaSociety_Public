87e68fa9e46e75574ed9dc21f50a3fb85a55820b
Amshitha-Nair <amshithanair0@gmail.com>
2024-02-10 13:58:31 +0530

    Update AccountingGroup.js

--- src/Screens/AccountingGroup.js
+++ src/Screens/AccountingGroup.js
@@ -1,32 +1,58 @@
-// import * as React from 'react';
+import React, { useState } from 'react';
+import { InnerDisplay, CustomPaper, HighLight } from '../Components/Wrapper';
+import BreadCrumb from '../Components/BreadCrumbs.js';
+import SubHeader from '../Components/SubHeader';
+import '../Css/AccountingGroup.css';
+// import SocietyInfo from './SocietyInfo.js';
+import AcGTable from '../Components/AcGTable.js';
+// import ModifyButton from '../Components/ModifyButton';
+import BillingDetails from './BillingDetails.js';
 
-// import SearchBar from '../Components/SearchBar.js';
-// import {  CustomPaper, HighLight, InnerDisplay  } from '../Components/Wrapper.js';
-// import Pager from '../Components/Wrapper.js';
-// import { styled } from '@mui/material/styles';
-// import Stack from '@mui/material/Stack';
-// // import Paper from '@mui/material/Paper';
-// import BreadCrumb from '../Components/BreadCrumbs.js';
-// import '../Css/accounting_groups.css'
-// const AccountingGroup = () => {
-//   return (
-//     <InnerDisplay>
-//       <BreadCrumb
-//         link1='Administrator'
-//         link2='Accounting Groups'
-//         path1='/Home/'
-//         path2='/Home/AccountingGroup'
-//       />
-//       <CustomPaper>
-//         <Stack direction='row' spacing={2}>
-//           <CustomPaper elevation={3} variant='elevation'>
-//             <HighLight KeyWord='Accounting Groups' />
-//             <SearchBar placeholder='    Search User ID/ User Name' />
-//           </CustomPaper>
-//         </Stack>
-//       </CustomPaper>
-//     </InnerDisplay>
-//   );
-// };
+const AccountingGroup = () => {
+  const [subBody, setSubBody] = useState(Array(2).fill(false));
 
-// export default AccountingGroup;
+  const handleSubBody = (index) => {
+    setSubBody((prevSubBody) => {
+      const newSubBody = [...prevSubBody];
+      newSubBody[index] = !newSubBody[index];
+      return newSubBody;
+    });
+  };
+  const [SocForm,setSocForm] = useState('')
+  const getSocForm =(socInfo)=>{
+    setSocForm(socInfo)
+  }
+  return (
+    <InnerDisplay>
+      <BreadCrumb
+        link1='Administrator'
+        link2='Accounting Groups'
+        path1='/Home/'
+        path2='/Home/'
+      />
+      <CustomPaper>
+        <HighLight KeyWord='Accounting Groups' />
+        <div className='selectBtn'>
+        <div  onClick={() => handleSubBody(1)}>
+          <SubHeader  barname={'Assets'} openStatus={subBody[1]} />
+        </div>
+        {subBody[1] && <AcGTable/>}
+        <div onClick={() => handleSubBody(2)}>
+          <SubHeader barname={'Liability'} openStatus={subBody[2]}/>
+        </div>
+        {subBody[2] && <AcGTable />}
+        <div  onClick={() => handleSubBody(3)}>
+          <SubHeader barname={'Income'} openStatus={subBody[3]} /> 
+        </div>
+        {subBody[3] && <AcGTable />}
+        <div onClick={() => handleSubBody(4)}>
+          <SubHeader barname={'Expenditure'} openStatus={subBody[4]}/>
+        </div>
+        {subBody[4] && <AcGTable />}
+        </div>
+      </CustomPaper>
+    </InnerDisplay>
+  );
+};
+
+export default AccountingGroup;
