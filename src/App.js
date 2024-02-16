ba9d7a282ca81746e171fcb035b67a67925f6b99
exploregithubnature <88432145+Amshitha-Nair@users.noreply.github.com>
2024-02-16 20:04:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
             <Route path='/LogIn' element={<LogIn />} />
             <Route path='/ForgotPassword' element={<ForgotPassword />} />
             <Route path='/Home' element={<HomeScreen />}>
-              <Route path='/Home/YourData' element={<YourData />} />
+              <Route path='/Home/' element={<YourData />} />
               <Route path='/Home/UserUnlock' element={<UserUnlock />} />
               <Route path='/Home/UserInfo' element={<UserInfo />} />
               <Route path='/Home/AdminUserLogin' element={<AdminUserLogin />} />
@@ -67,7 +67,6 @@ function App() {
               <Route path='/Home/BillingHeads' element={<BillingHeads />} />
               <Route path='/Home/ModifyBilling' element={<ModifyBilling />} />
             </Route>
-
             <Route path='/MainScreen' element={<MainScreen />} />
           </Routes>
         </div>
